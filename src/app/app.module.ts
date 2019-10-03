56772531317ae40ac792f09d3e73b152625793e4
Kevin Pageau <pageau.kevin@gmail.com>
2019-10-03 18:13:39 -0400

    Add fade in on game canvas after initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
@@ -21,6 +22,7 @@ import { GameComponent } from "./game/game.component";
         GameComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule
     ],
